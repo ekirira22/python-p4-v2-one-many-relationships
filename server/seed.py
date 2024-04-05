1c0d5db1854d5c1d9ff3063fbb2d9d40d454b818
Eric Maranga <ekirira22@gmail.com>
2024-04-05 22:09:33 +0300

    changed back_populate to backref, updated seed data, works

--- server/seed.py
+++ server/seed.py
@@ -4,6 +4,8 @@
 import datetime
 from app import app
 from models import db, Employee, Review, Onboarding
+from faker import Faker
+from random import choice as rc
 
 with app.app_context():
     # Delete all rows in tables
@@ -11,39 +13,33 @@ with app.app_context():
     Review.query.delete()
     Onboarding.query.delete()
 
-    # Add model instances to database
-    uri = Employee(name="Uri Lee", hire_date=datetime.datetime(2022, 5, 17))
-    tristan = Employee(name="Tristan Tal", hire_date=datetime.datetime(2020, 1, 30))
-    db.session.add_all([uri, tristan])
+    # faker generator
+    fake = Faker()
+
+    # Add employee model instances to database
+    employees = []
+    onboarding = []
+    days = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28]
+    months = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+    for n in range(10):
+        # Hire employee
+        n_employee = Employee(name=fake.first_name(), hire_date = datetime.datetime(2022, rc(months), rc(days)))
+        employees.append(n_employee)
+
+        # Onboarding employee
+        n_onboarding = Onboarding(orientation=datetime.datetime(2022, rc(months), rc(days)), employee=n_employee)
+        onboarding.append(n_onboarding)
+
+    db.session.add_all(employees)
+    db.session.add_all(onboarding)
     db.session.commit()
 
-    # 1..many relationship between Employee and Review
-    uri_2023 = Review(year=2023,
-                    summary="Great web developer!",
-                    employee = uri)
-    tristan_2021 = Review(year=2021,
-                        summary="Good coding skills, often late to work",
-                        employee = tristan)
-    tristan_2022 = Review(year=2022,
-                        summary="Strong coding skills, takes long lunches",
-                        employee = tristan)
-    tristan_2023 = Review(year=2023,
-                        summary="Awesome coding skills, dedicated worker",
-                        employee = tristan)
-    db.session.add_all([uri_2023, tristan_2021, tristan_2022, tristan_2023])
-    db.session.commit()
+    # Add review model instances to database
+    reviews = []
+    for n in range(25):
+        n_review = Review(year=2023, summary=fake.text(), employee=rc(employees))
+        reviews.append(n_review)
 
-    # 1..1 relationship between Employee and Onboarding
-
-    uri_onboarding = Onboarding(
-        orientation=datetime.datetime(2023, 3, 27),
-        employee=uri)
-
-    tristan_onboarding = Onboarding(
-        orientation=datetime.datetime(2020, 1, 20, 14, 30),
-        forms_complete=True,
-        employee=tristan)
-
-    db.session.add_all([uri_onboarding, tristan_onboarding])
-
+    db.session.add_all(reviews)
     db.session.commit()
